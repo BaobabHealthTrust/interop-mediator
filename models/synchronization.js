40d31863e78199153da376b4e2222041160be1b5
mmalumbo <mmalumbo@gmail.com>
2018-10-18 16:47:58 +0200

    added synchronization schema.
    Signed-off-by: mmalumbo <mmalumbo@gmail.com>

--- models/synchronization.js
+++ models/synchronization.js
@@ -1,25 +1,46 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
 
+const genericStringType = { type: String, required: true };
+const genericNumberType = { type: Number, required: true };
+const genericDateType = { type: Date, default: Date.now };
+
+const customGenericStringType = {
+  type: { type: String, default: "String" },
+  previousValue: String,
+  newValue: String
+};
+
+const customGenericDateType = {
+  type: { type: String, default: "Date" },
+  previousValue: Date,
+  newValue: Date
+};
+
+const synchronizationFacilitiesFields = {
+  name: customGenericStringType,
+  CommonName: customGenericStringType,
+  Code: customGenericStringType,
+  OperationalStatus: customGenericStringType,
+  RegulatoryStatus: customGenericStringType,
+  DateOpened: customGenericDateType,
+  LastUpdated: customGenericDateType,
+  DHIS2Code: customGenericStringType,
+  OpenLMISCode: customGenericStringType,
+  District: customGenericStringType,
+  Zone: customGenericStringType,
+  isCreated: Boolean,
+  isRemoved: Boolean
+};
+
 const synchronizationSchema = new Schema({
-  totalFacilitiesAdded: {
-    type: Number,
-    required: "Please provide total facilities added"
-  },
-  totalFacilitiesRemoved: {
-    type: Number,
-    required: "Please provide total facilities removed"
-  },
-  totalFacilitiesUpdated: {
-    type: Number,
-    required: "Please provide total facilities updated"
-  },
-  synchronizationDate: {
-    type: Date,
-    default: Date.now
-  },
-  clientId: String
+  totalFacilitiesAdded: genericNumberType,
+  totalFacilitiesRemoved: genericNumberType,
+  totalFacilitiesUpdated: genericNumberType,
+  synchronizationDate: genericDateType,
+  clientId: genericStringType,
+  isSuccessful: Boolean,
+  facilities: [synchronizationFacilitiesFields]
 });
 
 module.exports = mongoose.model("Synchronization", synchronizationSchema);
