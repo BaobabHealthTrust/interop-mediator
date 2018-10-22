f6cf12179d098edbf9ff28f03edc3d9bff4f97ba
mmalumbo <mmalumbo@gmail.com>
2018-10-22 14:58:54 +0200

    change

--- controllers/changedFacilitiesController.js
+++ controllers/changedFacilitiesController.js
@@ -38,15 +38,61 @@ module.exports = async (req, res) => {
     };
   };
 
-  const preparedMediatorFacilities = lastSynchronizationFacilities.map(
-    prepareFacilities
-  );
+  const mf = lastSynchronizationFacilities.map(prepareFacilities);
 
-  // const MHFR_URL = `${process.env.MFL_API_URL}/api/Facilities`;
-  // const MHFRFacilities = await request.get(MHFR_URL);
+  const query = JSON.stringify({
+    include: ["regulatoryStatus", "operationalStatus", { district: "zone" }],
+    limit: 1
+  });
+
+  const MHFR_URL = `${process.env.MFL_API_URL}/api/Facilities?filter=${query}`;
+  const { data: facilities } = await request.get(MHFR_URL);
   // _.isEqual(array1.sort(), array2.sort()); compare two array
 
-  console.log(preparedMediatorFacilities);
+  const pyo = facility => {
+    const {
+      facility_code: Code = null,
+      facility_code_dhis2: DHIS2Code = null,
+      facility_code_openlmis: OpenLMISCode = null,
+      registration_number,
+      facility_name: Name = null,
+      common_name: commonName,
+      facility_date_opened: DateOpened = null,
+      updated_at: LastUpdated = null,
+      district = null,
+      regulatoryStatus = null,
+      operationalStatus = null
+    } = facility;
+
+    const { district_name: District = null, zone } = district;
+    const Zone = zone.zone_name;
+
+    const {
+      facility_regulatory_status: RegulatoryStatus = null
+    } = regulatoryStatus;
+
+    const {
+      facility_operational_status: OperationalStatus = null
+    } = operationalStatus;
+
+    zone_name: return {
+      Name,
+      commonName,
+      Code,
+      OperationalStatus,
+      RegulatoryStatus,
+      DateOpened,
+      LastUpdated,
+      DHIS2Code,
+      OpenLMISCode,
+      District,
+      Zone
+    };
+  };
+
+  const qf = facilities.map(pyo);
+  console.log(mf, qf);
+
   const message = "Get Last synchronizations form database";
   OrchestrationRegister.add(req, message, "malu", 200);
 
