1e3a814b4bb1fe5bc46f375736928f623eeeb793
mmalumbo <mmalumbo@gmail.com>
2018-10-24 04:45:34 +0200

    added a winston error log
    Signed-off-by: mmalumbo <mmalumbo@gmail.com>

--- middleware/validateId.js
+++ middleware/validateId.js
@@ -1,8 +1,10 @@
+const { error } = require("winston");
 const mongoose = require('mongoose');
 
 module.exports = function (req, res, next) {
   if (!mongoose.Types.ObjectId.isValid(req.params.id)){
-    return res.status(404).send('Invalid ID.');
+    error('Invalid param ID.')
+    return res.status(404).send('Invalid param ID.');
   }
 
   next();
