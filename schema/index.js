0a5a7f0a9d348e0e88031be6e7db99fc5602ad51
mmalumbo <mmalumbo@gmail.com>
2018-10-24 04:14:27 +0200

    added a schema package.
    Signed-off-by: mmalumbo <mmalumbo@gmail.com>

---
+++ schema/index.js
@@ -0,0 +1,1 @@
+module.exports.synchronizationSchema = require('./synchronization')
---
+++ schema/synchronization.js
@@ -0,0 +1,37 @@
+const Joi = require("joi");
+
+const facilityFieldStringValidation = {
+  previousValue: Joi.string().allow(null).required(),
+  newValue: Joi.string().allow(null).required()
+};
+
+const facilityFieldDateValidation = {
+  previousValue: Joi.date().allow(null).required(),
+  newValue: Joi.date().allow(null).required()
+};
+
+const facilitiesSchema = Joi.object().keys({
+  Name: Joi.object().keys(facilityFieldStringValidation),
+  CommonName: Joi.object().keys(facilityFieldStringValidation),
+  Code: Joi.object().keys(facilityFieldStringValidation),
+  OperationalStatus: Joi.object().keys(facilityFieldStringValidation),
+  RegulatoryStatus: Joi.object().keys(facilityFieldStringValidation),
+  DateOpened: Joi.object().keys(facilityFieldDateValidation),
+  LastUpdated: Joi.object().keys(facilityFieldDateValidation),
+  DHIS2Code: Joi.object().keys(facilityFieldStringValidation),
+  OpenLMISCode: Joi.object().keys(facilityFieldStringValidation),
+  District: Joi.object().keys(facilityFieldStringValidation),
+  Zone: Joi.object().keys(facilityFieldStringValidation),
+  isNew: Joi.boolean().required(),
+  isRemoved: Joi.boolean().required()
+});
+
+module.exports.idSchema = Joi.object().keys({ id: Joi.objectId().required() });
+
+module.exports.addSchema = Joi.object().keys({
+  totalFacilitiesAdded: Joi.number().positive().required(),
+  totalFacilitiesRemoved: Joi.number().positive().required(),
+  totalFacilitiesUpdated: Joi.number().positive().required(),
+  isSuccessful: Joi.boolean().valid([true, false]).required(),
+  facilities: Joi.array().items(facilitiesSchema).required()
+});
